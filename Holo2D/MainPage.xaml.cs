72872f150123639f7ba137a638a4dd50761c480e
Kevin Evans <kwevans@gmail.com>
2017-04-01 23:39:16 +0100

    intial code

--- Holo2D/MainPage.xaml.cs
+++ Holo2D/MainPage.xaml.cs
@@ -1,30 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Holo2D
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+   
+    public class Poster
+    {
+        public async Task LoadFromFileAsync(StorageFile file)
+        {
+            this.image = new BitmapImage();
+            using (var stream = await file.OpenReadAsync())
+            {
+                this.image.SetSource(stream);
+            }
+        }
+        public BitmapImage Image
+        {
+            get
+            {
+                return (this.image);
+            }
+        }
+        BitmapImage image;
+    }
     public sealed partial class MainPage : Page
     {
+        public ObservableCollection<Poster> Posters
+        {
+            get; set;
+        }
+
         public MainPage()
         {
             this.InitializeComponent();
+            this.Posters = new ObservableCollection<Holo2D.Poster>();
+            this.Loaded += OnLoaded;
+
+        }
+
+        async void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var folder = await Package.Current.InstalledLocation.GetFolderAsync("images");
+            var files = await folder.GetFilesAsync();
+
+            foreach (var file in files)
+            {
+                var poster = new Poster();
+                await poster.LoadFromFileAsync(file);
+                this.Posters.Add(poster);
+            }
         }
     }
 }
